f5b600e8415a570cc1d7396960f225aa936afcfa
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2025-06-06 15:05:04 -0700

    copy

---
+++ app/components/echo.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import ReactPlayer from "react-player";
+import { useState } from "react";
+
+export default function Home() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isInteracting, setIsInteracting] = useState(false);
+
+  const handleTogglePlaying = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleToggleInteracting = () => {
+    setIsInteracting(!isInteracting);
+  };
+
+  // const handleKeyDown = (event: KeyboardEvent) => {
+  //   if (event.key === " ") {
+  //     handleToggleInteracting();
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener("keydown", handleKeyDown);
+  //   return () => window.removeEventListener("keydown", handleKeyDown);
+  // }, []);
+
+  // Render a YouTube video player
+  return (
+    <div className="flex flex-col h-screen w-screen items-center justify-center">
+      <ReactPlayer
+        url="https://ucarecdn.com/398f91ab-6353-4ecc-ac4f-80a7a657c392/"
+        playing={isPlaying}
+        // loop={true}
+        // muted={true}
+        // width="100%"
+        // height="100%"
+      />
+      <button onClick={handleTogglePlaying}>
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+      <button onClick={handleToggleInteracting}>
+        {isInteracting ? "Stop" : "Interact"}
+      </button>
+    </div>
+  );
+}
