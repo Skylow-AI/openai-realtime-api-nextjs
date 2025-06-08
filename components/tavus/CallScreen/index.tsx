88eedafe6d4734c7ebe56ca1a7c9fa148e2b17a6
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2025-06-08 10:26:08 -0700

    good

--- components/tavus/CallScreen/index.tsx
+++ components/tavus/CallScreen/index.tsx
@@ -44,12 +44,12 @@ export const CallScreen = ({
         <div className={isInteracting ? "" : "hidden"}>
           <Call />
         </div>
-        <CameraSettings actionLabel="Leave Call" onAction={handleLeave} />
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <button onClick={handleToggleInteracting}>
-          {isInteracting ? "Stop" : "Interact"}
-        </button>
+        <div className="fixed bottom-0 left-0 right-0 z-50 flex flex-col justify-center items-center">
+          <CameraSettings actionLabel="Leave Call" onAction={handleLeave} />
+          <button onClick={handleToggleInteracting}>
+            {isInteracting ? "Stop" : "Interact"}
+          </button>
+        </div>
       </div>
     </>
   );
